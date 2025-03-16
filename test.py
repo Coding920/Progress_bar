58903d312277bbb46a0a0944c3245bc8d0b02a74
coding920 <justin.tamez04@gmail.com>
2025-03-16 08:43:57 -0700

    Modified tests to be random

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
 import time
+import random
 from progressbar import *
 
 progressbar = ProgressBar()
 
-messages = ["loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "loading", "uploading", "downloading", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            ]
+messages = ["loading", "uploading", "downloading", "unzipping", "zipping", "processing", "compiling",
+            "loading", "uploading", "downloading", "unzipping", "zipping", "processing", "compiling",
+            "loading", "uploading", "downloading", "unzipping", "zipping", "processing", "compiling",
+            "loading", "uploading", "downloading", "unzipping", "zipping", "processing", "compiling",
+            "loading", "uploading", "downloading", "unzipping", "zipping", "processing", "compiling",
+            "loading", "uploading", "downloading", "unzipping", "zipping", "processing", "compiling",
+            "loading", "uploading", "downloading", "unzipping", "zipping", "processing", "compiling",
+            "loading", "uploading", "downloading", "unzipping", "zipping", "processing", "compiling",
+            "loading", "uploading", "downloading", "unzipping", "zipping", "processing", "compiling",
+            "loading", "uploading", "downloading", "unzipping", "zipping", "processing", "compiling",
+            "loading", "uploading", "downloading", "unzipping", "zipping", "processing", "compiling",
+]
+length = len(messages)
 
 progressbar.prep()
-for i in range(1, 51):
-    progressbar.display(i, 50, messages[i])
-    time.sleep(0.25)
+for idx, msg in enumerate(messages):
+    progressbar.display(idx, length, msg)
+    time.sleep(random.uniform(0.25, 1.5))
 progressbar.end_display()
